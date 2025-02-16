dcd291cb5cf8c8f6995df1aacd636e842bf7c5de
Neha Sagare <nehasagare40@gmail.com>
2025-02-16 21:00:54 +0530

    Practical1_Hashing.py

--- Practical1_Hashing.py
+++ Practical1_Hashing.py
@@ -1,9 +1,9 @@
- 
-# PROBLEM STATEMENT: 
+"""
+PROBLEM STATEMENT: 
 
-# Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone number. 
-# Make use of two collision handling techniques and compare them using number of comparisons required to find a set of telephone numbers.
- 
+Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone number. 
+Make use of two collision handling techniques and compare them using number of comparisons required to find a set of telephone numbers.
+""" 
 
 #INPUT:
 
